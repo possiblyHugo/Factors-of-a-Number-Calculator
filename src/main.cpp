695694ef7fbea4156c8b1d67ad7b8f92457628c4
possiblyHugo <alainhandzar@gmail.com>
2024-02-25 22:59:13 -0800

    + Initial prompt + Added algorithm + Added output

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 
 int main() {
 	int base_number = 0;
+	
 	std::cout << "Input the base number: ";
 	std::cin >> base_number;
-
-
+	
+	for (int i = 1; i <= base_number; i++) {
+		if (i > base_number / 2) {
+			std::cout << base_number;
+			break;
+		} 
+		else if ((base_number % i) == 0) {
+			std::cout << i << ", ";
+		}
+	}
 
 	return 0;
 }
